313a264b847094d1c06b040ab754546f7aedf87b
Elias Forte <eliasforte.dev@gmail.com>
2022-05-24 11:55:59 -0300

    feat: add url for development

--- src/api/server.ts
+++ src/api/server.ts
@@ -5,6 +5,7 @@ import App from './app';
 dotenv.config();
 
 const app = new App();
-mongoose.connect(`${process.env.MONGO_URL}`);
+const databaseUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URL : process.env.MONGODB_DEV_URL;
+mongoose.connect(databaseUrl);
 
 app.server(process.env.PORT || 3000);
