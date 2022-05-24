0849d6fa53772af880a8218d9cbb5db029b67d22
Elias Forte <eliasforte.dev@gmail.com>
2022-05-24 12:42:55 -0300

    fix: atritube schema to document

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,33 +1,33 @@
 import { Model as ModelDoc } from 'mongoose';
 
 export default abstract class Model<T> {
-  protected schema: ModelDoc<T>;
+  protected document: ModelDoc<T>;
 
   constructor(schema: ModelDoc<T>) {
-    this.schema = schema;
+    this.document = schema;
   }
 
   public async create(infos: T): Promise<T> {
-    return this.schema.create(infos);
+    return this.document.create(infos);
   }
 
   public async findAll(): Promise<T[]> {
-    return this.schema.find();
+    return this.document.find();
   }
 
   public async findOne(id: string): Promise<T | null> {
-    return this.schema.findById(id);
+    return this.document.findById(id);
   }
 
   public async findByEmail(email: string): Promise<T | null> {
-    return this.schema.findOne({ email });
+    return this.document.findOne({ email });
   }
 
   public async update(id: string, infos: T): Promise<T | null> {
-    return this.schema.findByIdAndUpdate(id, infos, { new: true });
+    return this.document.findByIdAndUpdate(id, infos, { new: true });
   }
 
   public async delete(id: string): Promise<T | null> {
-    return this.schema.findByIdAndDelete(id);
+    return this.document.findByIdAndDelete(id);
   }
 }
