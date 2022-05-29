212b28a6a672281616f7202b3abe578275da1dba
Elias Forte <eliasforte.dev@gmail.com>
2022-05-29 11:18:19 -0300

    feat: create `Login` class into service folder

---
+++ src/services/Login.ts
@@ -0,0 +1,26 @@
+import bcrypt from 'bcrypt';
+import Model from '../models/Model';
+import UserModel from '../models/User';
+import Auth from '../utils/auth/Token';
+import { IUser } from '../utils/interfaces/IUser';
+import { USER_NOT_EXIST, INCORRECT_USER } from '../utils/errors';
+
+export default class LoginServce {
+  private _model: Model<IUser>;
+
+  constructor(model: Model<IUser> = new UserModel()) {
+    this._model = model;
+  }
+
+  public login = async (email: string, password: string): Promise<string> => {
+    const user = await this._model.findByEmail(email);
+    if (!user) throw USER_NOT_EXIST;
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+    if (!isPasswordValid) throw INCORRECT_USER;
+
+    const token = Auth.createToken({ _id: user._id, email, name: user.name });
+
+    return token;
+  };
+}
