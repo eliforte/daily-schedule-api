3ec42fb809a90f0d64f21411c98903443a7a275f
Elias Forte <eliasforte.dev@gmail.com>
2022-05-31 19:01:33 -0300

    feat: craeted schemas

---
+++ src/utils/schemas/index.ts
@@ -0,0 +1,22 @@
+import Joi from 'joi';
+
+export const SCHEMALogin = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+export const SCHEMAUser = Joi.object({
+  email: Joi.string().email().required(),
+  name: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+export const SCHEMATask = Joi.object({
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+});
+
+export const SCHEMAUpdateTask = Joi.object({
+  title: Joi.string(),
+  description: Joi.string().required(),
+});
