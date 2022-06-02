cb14bc3465604dc520527fb7776ca2a37ac13c94
Elias Forte <eliasforte.dev@gmail.com>
2022-06-02 17:45:33 -0300

    fix: return error middleware

--- src/middlewares/error/index.ts
+++ src/middlewares/error/index.ts
@@ -7,13 +7,13 @@ interface IErrorResponse extends ValidationError, IError {}
 export default class ErrorHandler {
   public static handler = (err: IErrorResponse, req: Req, res: Res): void => {
     if (err.isJoi) {
-      res.status(400).json(err.details[0].message);
+      res.status(400).json({ message: err.details[0].message });
     }
 
     if (err.status) {
-      res.status(err.status).json(err.message);
+      res.status(err.status).json({ message: err.message });
     }
 
-    res.status(500).json(err.message);
+    res.status(500).json({ message: err.message });
   };
 }
