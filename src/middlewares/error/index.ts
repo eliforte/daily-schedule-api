1d68986517138dbc46deabacc74d20ab6dcd8003
Elias Forte <eliasforte.dev@gmail.com>
2022-06-02 19:41:58 -0300

    fix: params function handler into class `ErrorHandler`

--- src/middlewares/error/index.ts
+++ src/middlewares/error/index.ts
@@ -1,11 +1,12 @@
-import { Request as Req, Response as Res } from 'express';
+/* eslint-disable no-unused-vars */
+import { NextFunction as Next, Request as Req, Response as Res } from 'express';
 import { ValidationError } from 'joi';
 import { IError } from '../../utils/interfaces/IError';
 
 interface IErrorResponse extends ValidationError, IError {}
 
 export default class ErrorHandler {
-  public static handler = (err: IErrorResponse, req: Req, res: Res) => {
+  public static handler = (err: IErrorResponse, req: Req, res: Res, next: Next) => {
     if (err.isJoi) {
       return res.status(400).json({ message: err.details[0].message });
     }
